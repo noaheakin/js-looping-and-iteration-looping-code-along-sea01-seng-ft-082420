fd845777bd387f84ea21491f2f54f9e31a19e817
noaheakin <noaheakin@gmail.com>
2020-10-05 11:27:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+function writeCards(names, type) {
+    let thanks = []
+    for (let i = 0; i < names.length; i++) {
+        thanks.push(`Thank you, ${names[i]}, for the wonderful ${type} gift!`)
+    }
+    return thanks
+}
+
+function countDown (int) {
+    while (int > 0) {
+        console.log(int);
+        int--;
+    }
+    console.log(int);
+}
